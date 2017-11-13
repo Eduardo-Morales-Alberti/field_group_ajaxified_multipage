7c6bed81cb656ae491f6a8e526c53403d5aaf230
rubentin <ruben.garcia@metadrop.net>
2017-11-13 15:36:14 +0100

    Fixed JS calls for D8
    In Drupal 8 you cannot add inline JS to an element's #attached array. It
    must be done via a library. I have changed the code so the JS is called
    and used as a library

--- js/field_group_ajaxified_multipage.js
+++ js/field_group_ajaxified_multipage.js
@@ -1,17 +1,17 @@
-(function($){
+/**
+ * @file
+ * field_group_ajaxified_multipage.js
+ */
 
-    /**
+(function ($, Drupal) {
+  /**
    * Scroll to top.
    */
-    Drupal.behaviors.scrollTop = {
-        attach: function (context, settings) {
-
-            $('html, body').animate(
-                {
-                    scrollTop: $("body").offset().top
-                }, 200
-            );
-
-        }
-    };
-})(jQuery);
+  Drupal.behaviors.scrollTop = {
+    attach: function (context, settings) {
+      $('html, body').animate({
+        scrollTop: $("body").offset().top
+      }, 200);
+    }
+  };
+})(jQuery, Drupal);
