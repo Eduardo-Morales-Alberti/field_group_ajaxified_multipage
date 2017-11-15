aaff35d8d7f857641d77f6fb297e9809f98d4f50
Eduardo <eduardo.morales@metadrop.net>
2017-11-15 12:38:57 +0100

    Fixed functionality scroll to top.

--- js/field_group_ajaxified_multipage.js
+++ js/field_group_ajaxified_multipage.js
@@ -4,20 +4,23 @@
   */
 
 (function ($, Drupal) {
-
   Drupal.behaviors.field_group_ajaxified_multipage = {
     attach: function (context, settings) {
-      scrollToTop(context);
+      scrollToTop('.field-group-ajaxifield-multipage-scroll', context);
     }
   };
 
   /**
    * Scroll to top.
    */
-   function scrollToTop(context){
-     $('html, body').animate({
-       scrollTop: $("body").offset().top
-     }, 200);
-   }
-
+  function scrollToTop(element, context) {
+    $(element).once('scroll-to-top').each(function () {
+      var $element = $(this);
+      if (!$element.hasClass('prevent-scroll') || $element.find('.error').length > 0) {
+        $('html, body').animate({
+          scrollTop: $element.offset().top - 20
+        }, 200);
+      }
+    });
+  }
 })(jQuery, Drupal);
